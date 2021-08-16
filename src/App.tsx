4126cb92042045dfdf8c392d461e619185a7ff64
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-08-16 16:43:28 +0530

    added input and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import './App.css';
@@ -6,8 +7,9 @@ import { actionCreators ,State} from './state';
 
 
 function App() {
-  const dispatch = useDispatch();
+  const [input,setInput] = useState(0);
 
+  const dispatch = useDispatch();
   const { depositMoney,withdrawMoney,bankruptMoney } = bindActionCreators(actionCreators,dispatch);
   const amount  = useSelector((state:State) => state.bank)
 
@@ -15,9 +17,16 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <h1>{amount}</h1>
-        <button onClick={() => withdrawMoney(500)}>withdraw</button>
-        <button onClick={() => depositMoney(1000)}>deposit</button>
-        <button onClick={() => bankruptMoney()}>bankrupt</button>
+        <input
+          type='number'
+          placeholder='enter amount'
+          onChange={(e) => setInput(parseInt(e.target.value))}
+        />
+        <div className='btns'>
+          <button onClick={() => withdrawMoney(input)}>withdraw</button>
+          <button onClick={() => depositMoney(input)}>deposit</button>
+          <button onClick={() => bankruptMoney()}>bankrupt</button>
+        </div>
       </header>
     </div>
   )
